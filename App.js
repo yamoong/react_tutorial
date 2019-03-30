ecdab8717f720cff9650b6a246845e1f2af6d557
kangbok-lee <gangbok119@gmail.com>
2019-03-30 12:40:47 +0900

    프리로더 에셋

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
 import React from 'react';
+import { AppLoading, Font } from 'expo';
+import { Ionicons } from '@expo/vector-icons';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
-}
+	state = {
+		loaded: true,
+	};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	handleError = error => console.log(error);
+	handleLoaded = () => this.setState({ loaded: true });
+
+	loadAssets = async () => {
+		await Font.loadAsync({
+			...Ionicons.font,
+		});
+	};
+
+	render() {
+		const { loaded } = this.state;
+		if (loaded) {
+			return (
+				<View style={StyleSheet.container}>
+					<Text>Open up App.js to start working on your app!</Text>
+				</View>
+			);
+		} else {
+			return <AppLoading startAsync={this.loadAssets} onFinish={this.handleLoaded} onError={this.handleError} />;
+		}
+	}
+}
